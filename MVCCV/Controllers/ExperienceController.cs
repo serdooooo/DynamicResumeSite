160e0466eb601f4b7d81f67198944b1c2c647fd4
serdooooo <karakurtserdar19@gmail.com>
2022-03-08 17:06:39 +0300

    Experience

--- MVCCV/Controllers/ExperienceController.cs
+++ MVCCV/Controllers/ExperienceController.cs
@@ -1,4 +1,5 @@
-﻿using MVCCV.Repositories;
+﻿using MVCCV.Models.Entity;
+using MVCCV.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +16,41 @@ namespace MVCCV.Controllers
         {
             var exp = repo.List();
             return View(exp);
+        }
+        public ActionResult ExperienceAdd()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ExperienceAdd(Experiences exp)
+        {
+            repo.TAdd(exp);
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult ExperienceDelete(int id)
+        {
+            Experiences t = repo.Find(x => x.ID == id);
+            repo.TDelete(t);
+            return RedirectToAction("Index");
+        }
+        public ActionResult ExperienceGet(int id)
+        {
+            Experiences t = repo.Find(x => x.ID == id);
+            return View(t);
+        }
+        [HttpPost]
+        public ActionResult ExperienceGet(Experiences ex)
+        {
+            Experiences t = repo.Find(x => x.ID == ex.ID);
+            t.Title = ex.Title;
+            t.Subtitle = ex.Subtitle;
+            t.ExperienceDate = ex.ExperienceDate;
+            t.Definition = ex.Definition;
+            repo.TUpdate(t);
+            return RedirectToAction("Index");
         }
     }
 }
--- MVCCV/Repositories/GenericRepository.cs
+++ MVCCV/Repositories/GenericRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Web;
 using MVCCV.Models.Entity;
 
@@ -31,6 +32,10 @@ namespace MVCCV.Repositories
         public void TUpdate(T t)
         {
             db.SaveChanges();
+        }
+        public T Find(Expression<Func<T, bool>> where)
+        {
+            return db.Set<T>().FirstOrDefault(where);
         }
     }
 }
